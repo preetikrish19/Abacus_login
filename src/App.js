8672dbbf0200505b4d730c1150814d71ab8325a3
Snehasuresh01 <66668157+Snehasuresh01@users.noreply.github.com>
2022-02-05 15:16:29 +0530

    scroll bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 import SignUp2 from "./pages/signup2";
-//import SignUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 import SignInForm from "./pages/SignInForm";
 import { Typography } from "@material-ui/core";
 import "./App.css";
@@ -25,43 +25,25 @@ class App extends Component {
         </div>
           <div className="appForm">
             <div className="pageSwitcher">
-              <NavLink
-                to="/sign-in"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
+              <NavLink to="/sign-in" activeClassName="pageSwitcherItem-active" className="pageSwitcherItem" >
                 Sign In
               </NavLink>
-              <NavLink
-                exact
-                to="/"
-                activeClassName="pageSwitcherItem-active"
-                className="pageSwitcherItem"
-              >
+              <NavLink exact to="/" activeClassName="pageSwitcherItem-active" className="pageSwitcherItem">
                 Sign Up
               </NavLink>
             </div>
 
             <div className="formTitle">
-              <NavLink
-                to="/sign-in"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
+              <NavLink to="/sign-in" activeClassName="formTitleLink-active" className="formTitleLink" >
                 Sign In
               </NavLink>{" "}
               or{" "}
-              <NavLink
-                exact
-                to="/"
-                activeClassName="formTitleLink-active"
-                className="formTitleLink"
-              >
+              <NavLink exact to="/" activeClassName="formTitleLink-active" className="formTitleLink">
                 Sign Up
               </NavLink>
             </div>
 
-            <Route exact path="/" component={SignUp2} />   {/* Signup form*/}
+            <Route exact path="/" component={SignUp} />   {/* Signup form*/}
             <Route path="/sign-in" component={SignInForm} />
           </div>
         </div>
