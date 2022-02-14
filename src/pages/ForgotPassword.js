f1cdb622a0a21c594739ec0930b3f187d10f6cb4
Snehasuresh01 <66668157+Snehasuresh01@users.noreply.github.com>
2022-02-14 19:53:15 +0530

    padding

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -20,34 +20,6 @@ function ForgotPassword() {
               onChange={this.handleChange}
             />
           </div>
-
-          <div className="formField">
-            <label className="formFieldLabel" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className="formFieldInput"
-              placeholder="Enter your password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
-
-          <div className="formField">
-            <button className="formFieldButton">Sign In</button>{" "}
-            <Link to="/sign-in" className="formFieldButtonDirected" >
-              Forgot Password?
-            </Link>
-            </div>
-            <div className="formField">
-            <Link to="/" className="formFieldLink">
-              Create an account
-            </Link>
-          </div>
-
         </form>
       </div>
     );
